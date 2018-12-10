6e86d6fb0c3829ca8f7728fd728251a743f7e69e
raissuror <raissabilullah@gmail.com>
2018-12-10 11:48:24 +0700

    update lagi

--- LutunaProject/application/views/homepage.php
+++ LutunaProject/application/views/homepage.php
@@ -12,49 +12,90 @@
             <div class="container-fluid">
                 <div class="navbar-header">
                     <a href="http://localhost/LutunaProject/">
-                        <img src="<?php echo base_url('assets\Icons\beruang.png'); ?>" id="bear">
+                        <img  id="bear" src="<?php echo base_url('assets\Icons\beruang.png'); ?>">
                     </a><!--icon beruang-->      
                 </div>
                 <div class="nav-centered">
-                    <h1 style="font: ;">LUTUNA</h1>
+                    <img src="<?php echo base_url('assets\Icons\LUTUNA.png'); ?>">
                 </div>
             </div>
         </nav>
+        <div>
+            <div class="row col-md-12">	<!--isi votingan yang sedang Hot-->              
+                <div style="background:"class="col-md-4">
+                    <div>
+                        <img id="bear_gif" src="<?php echo base_url('assets\Icons\beruang.gif'); ?>">
+                    </div>
+		</div>
+                <div style="background:grey" class="col-md-8">
+                    <div>
+                        <h1 id="wel">WELCOME</h1><br>
+                        <h1 id="to">TO</h1><br>
+                        <h1 id="LW">LUTUNA WEBSITE</h1>
+                    </div>
+		</div>
+            </div>	
+        </div>       
         <div class="col-md-12"><!--Voting yang sedang Hot-->
             <h1 id="hotnew">HOT</h1>
             <div class="row col-md-12">	<!--isi votingan yang sedang Hot-->
                 <?php foreach ($response as $data){?>               
                 <div class="col-md-2">
-                    <a href="http://localhost/LutunaProject/index.php/vote"><img style="height:auto; margin-left: auto; margin-right: auto" class="kotak bearhot" src=<?php echo base_url($data['img']); ?>></a>
-                    <br><br>
-                    <a href="http://localhost/LutunaProject/index.php/vote"><h4 style="font-family: verdana; color: white"id="judul_vote"><?php echo $data['judul']; ?></h4></a>
+                    <div class="crop">
+                        <a href="http://localhost/LutunaProject/index.php/vote">
+                            <img style="height:auto; margin-left: auto; margin-right: auto" src=<?php echo base_url($data['img']); ?>>
+                        </a>
+                        <br>
+                    </div>>
+                    <a href="http://localhost/LutunaProject/index.php/vote">
+                        <h4 style="font-size: 14px; font-family: verdana; color: white" id="judul_vote"><?php echo $data['judul']; ?></h4>
+                    </a>
                     <br>
 		</div>
                 <?php } ?>
-            </div>	<!--akhir voitngan yang sedang hot-->
+            </div>
+            <h1 id="hotnew">ALL VOTE</h1>
+            <div class="row col-md-12">	<!--isi votingan yang sedang Hot-->
+                <?php foreach ($response as $data){?>               
+                <div class="col-md-3">
+                    <div class="crop">
+                        <a href="http://localhost/LutunaProject/index.php/vote">
+                            <img style="height:auto; margin-left: auto; margin-right: auto" src=<?php echo base_url($data['img']); ?>>
+                        </a>
+                        <br>
+                    </div>>
+                    <a href="http://localhost/LutunaProject/index.php/vote">
+                        <h4 style="font-size: 14px; font-family: verdana; color: white" id="judul_vote"><?php echo $data['judul']; ?></h4>
+                    </a>
+                    <br>
+		</div>
+                <?php } ?>
+            </div>
         </div>                    
 	<div class="footer ">
-            <div class="row">
+            <div class="row">                
                 <div class="col-md-3">
                     <div class="col-md-12">
-                        <h1 id="menu">Link Page</h1>
-                        <a id="menu" href="http://localhost/LutunaProject/index.php/profile">Profil</a><br>
-                        <a id="menu" href="http://localhost/LutunaProject/index.php/register">Register</a><br>
-                        <a id="menu" href="http://localhost/LutunaProject/index.php/login">Login</a>
-                        
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="col-md-12">
-                        <h1 id="menu">Menu</h1>
-                        <a>ya</a>
+                        <h1 id="menu_bawah">About us</h1>
+                        <p id="menu_bawah">Lutuna adalah sebuah website yang diciptakan saat penciptanya diberi tugas 
+                           oleh dosennya untuk membat sebuah web. Lutuna bergerak dengan tujuan entertaining dan juga 
+                           mengisi waktu luang anda.
+                        </p>
                     </div>
                 </div>
                 <div class="col-md-3">    
                     <div class="col-md-12">
-                        <h1 id="menu">Menu</h1>
+                        <h1 id="menu_bawah">Menu</h1>
                         <a>ya</a>
                     </div>		
+                </div>
+                <div class="col-md-3">
+                    <div class="col-md-12">
+                        <h1 id="menu_bawah">Link Page</h1>
+                        <a id="menu_bawah" href="http://localhost/LutunaProject/index.php/profile">Profil</a><br>
+                        <a id="menu_bawah" href="http://localhost/LutunaProject/index.php/register">Register</a><br>
+                        <a id="menu_bawah" href="http://localhost/LutunaProject/index.php/login">Login</a>                        
+                    </div>
                 </div>
                     <div class="col-md-3">
                         <div class="col-md-12 row">
