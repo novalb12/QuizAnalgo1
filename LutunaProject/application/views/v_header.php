bb32a1f58cc38e742674327cf0cb1d4308fddcb2
novalb12 <novalb65@gmail.com>
2018-12-11 11:44:12 -0800

    rusak

---
+++ LutunaProject/application/views/v_header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		<nav class="navbar navbar-inverse navbar-fixed-top header">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a href="http://localhost/LutunaProject/">
+                        <img  id="bear" src="<?php echo base_url('assets\Icons\beruang.png'); ?>">
+                    </a><!--icon beruang-->      
+                </div>
+                <div class="nav-centered">
+                    <img src="<?php echo base_url('assets\Icons\LUTUNA.png'); ?>">
+                </div>
+            </div>
+        </nav>
+	</body>
+</html>
